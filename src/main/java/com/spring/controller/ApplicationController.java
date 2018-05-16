7add491b5d5ae7f7a25380e5e07db7b5f30b9b4b
hari-rayapaneni <hari.rayapaneni@gmail.com>
2018-05-16 11:41:39 +0530

    Enabled with @EnableWebMvc

--- src/main/java/com/spring/controller/ApplicationController.java
+++ src/main/java/com/spring/controller/ApplicationController.java
@@ -4,13 +4,15 @@ package com.spring.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Controller
+
+@EnableWebMvc
+@RestController
 public class ApplicationController {
 	
 	@RequestMapping(value="/users", method=RequestMethod.GET)
